961074886460c211564bba91494419f295223f2c
gnemoka <gnemo.ka@gmail.com>
2018-09-12 09:44:46 +0200

    Implementing basics functionnality; migration to version 1.1.0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+'use strict';
+
+const { Actor, Category, Channel, Dvd, Video } = require('./lib/doggyx');
+
+const v = new Video();
+v.find({})
+ .then(res => console.info(res))
+ .catch(err => console.log(err));
+
 module.exports = require('./lib/doggyx');
