cafd9bce82eb21121fddfa130185ad0d8a7935fa
gnemoka <gnemo.ka@gmail.com>
2018-09-23 01:29:38 +0200

    full refactorisation

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const { Actor, Category, Channel, Dvd, Video } = require('./lib/doggyx');
-
-const v = new Video();
-v.find({})
- .then(res => console.info(res))
- .catch(err => console.log(err));
-
-module.exports = require('./lib/doggyx');
+module.exports = require('./lib');
